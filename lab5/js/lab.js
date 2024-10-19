f4dcacf6cbc1c63236746beac50d2d6f949ba903
ColbyDaoJensen <c.daojensen@gmail.com>
2024-10-18 23:45:17 -0700

    Lab 5 Updates

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - make data types and variables
+// Author: Colby DaoJensen
+// Date: 10/21/24
 
-// Constants
+// Define Variables
+make = "Ford";
+model = "Taurus";
+color = "Red";
+year = "2004";
+ownIt = false;
 
-// Functions
+// Calculate
+age = 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Do I own it? " + ownIt + "<br>");
+document.writeln("Age: " + age + " years old.<br>");

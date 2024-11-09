1b22633653888c5be89605c7ab3b39804716ed4a
ColbyDaoJensen <c.daojensen@gmail.com>
2024-11-08 23:23:05 -0800

    Lab 11 Updates for JS Code

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -4,20 +4,20 @@
 // Author: Colby DaoJensen
 // Date: 11/11/24
 
-// Sorts the characters of a string in alphabetical order.
+// sortString - sorts the characters of a string in alphabetical order.
 function sortString(inputString) {
-    // We have to convert our string to an array and back again to sort it
+    // converts our string to an array and back again to sort it
     return inputString.split('').sort().join('');
 }
 
 // click listener for button
 $("#submit").click(function(){
-    // get value of input field
+    // gets value of input field
     const userName = $("#user-name").val();
     
-    // now let's sort it
+    // sorts it
     userNameSorted = sortString(userName);
 
-    // append a new div to our output div
+    // appends a new div to our output div
     $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
 });

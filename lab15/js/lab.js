5c64dfd5f43d6c221d77a30c86862acb9723ba9e
ColbyDaoJensen <c.daojensen@gmail.com>
2024-11-22 01:19:29 -0800

    Lab 15 Start

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,28 +1,35 @@
-// index.js - anon function and callbacks
+// index.js - AJAX
 // Author: Colby DaoJensen
-// Date: 10/31/24
+// Date: 11/25/24
 
-// addThree - adds the number 3 to the value x in the function
-function addThree(x){
-    var results = x + 3;
-    return results;
-}
+URL = "https://yesno.wtf/#api";
 
-// testing function
-console.log("What's 5 plus 3? ", addThree(5));
-console.log("What's 7 plus 3? ", addThree(7));
-
-array = [30, 70, 40, 80, 21];
-console.log("My array: ", array);
-
-var result = array.map(addThree);
-// should return [33, 73, 43, 83, 24]
-console.log("Adding 3 to the array: ", addThree(result));
-
-var result = array.map(function(x){
-    var results = x - 10;
-    return results;
+$('activate').click(function(){
+    // Using the core $.ajax() method
+    $.ajax({
+        // The URL for the request (from the api docs)
+        url: URL,
+        // The data to send (will be converted to a query string)
+        data: { 
+                // here is where any data required by the api 
+                //   goes (check the api docs)
+                id: 123,
+                api_key: "blahblahblah",
+            },
+        // Whether this is a POST or GET request
+        type: "GET",
+        // The type of data we expect back
+        dataType : "json",
+        // What do we do when the api call is successful
+        //   all the action goes in here
+        success: function(data) {
+            // do stuff
+            console.log(data);
+        },
+        // What we do if the api call fails
+        error: function (jqXHR, textStatus, errorThrown) { 
+            // do stuff
+            console.log("Error:", textStatus, errorThrown);
+        }
+    })
 });
-
-// should return [20, 60, 30, 70, 11]
-console.log("Subtracting 10 from the array: ", result);

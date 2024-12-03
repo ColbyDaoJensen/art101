1175c70defec3f1018ff41de1cb10e6e58e103cd
ColbyDaoJensen <c.daojensen@gmail.com>
2024-12-02 19:21:26 -0800

    Lab 16 Update

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,28 +1,39 @@
-// index.js - anon function and callbacks
+// index.js - JSON and APIs
 // Author: Colby DaoJensen
 // Date: 12/2/24
 
-// addThree - adds the number 3 to the value x in the function
-function addThree(x){
-    var results = x + 3;
-    return results;
+// Constants
+comicNum = 420;
+
+// Functions
+function getComic(comicNum)
+{
+    $.ajax({
+        // The URL for the request (from the api docs)
+        url: 'https://xkcd.com/${comicNum}/info.0.json',
+        // The data to send (will be converted to a query string)
+        data: { 
+                // here is where any data required by the api 
+                //   goes (check the api docs)
+            },
+        // Whether this is a POST or GET request
+        type: "GET",
+        // The type of data we expect back
+        dataType : "json",
+        // What do we do when the api call is successful
+        //   all the action goes in here
+        success: function(data) {
+            // do stuff
+            comicObj = data;
+            $("#title").html(comicObj.title);
+            $("#comicPhoto").html(`<img src="${comicObj.img}" alt="${comicObj.alt}" title="${comicObj.alt}"/>`);
+            $("#photoText").html(`<p>${comicObj.alt}</p>`);
+            console.log(data);
+        },
+        // What we do if the api call fails
+        error: function (jqXHR, textStatus, errorThrown) { 
+            // do stuff
+            console.log("Error:", textStatus, errorThrown);
+        }
+    });
 }
-
-// testing function
-console.log("What's 5 plus 3? ", addThree(5));
-console.log("What's 7 plus 3? ", addThree(7));
-
-array = [30, 70, 40, 80, 21];
-console.log("My array: ", array);
-
-var result = array.map(addThree);
-// should return [33, 73, 43, 83, 24]
-console.log("Adding 3 to the array: ", addThree(result));
-
-var result = array.map(function(x){
-    var results = x - 10;
-    return results;
-});
-
-// should return [20, 60, 30, 70, 11]
-console.log("Subtracting 10 from the array: ", result);
